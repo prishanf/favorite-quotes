093060d75d857620c8287ff58124182c461154f7
Prishan Fernando <prishanf@yahoo.com>
2018-03-25 09:06:33 -0700

    Delete the Home Page component,  Add new Page components to App module and clean up code.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,8 @@
+import { SettingsPage } from './../pages/settings/settings';
+import { QuotePage } from './../pages/quote/quote';
+import { QuotesPage } from './../pages/quotes/quotes';
+import { LibraryPage } from './../pages/library/library';
+import { FavoritesPage } from './../pages/favorites/favorites';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
@@ -5,13 +10,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
-  ],
+    FavoritesPage,
+    LibraryPage,
+    QuotesPage,
+    QuotePage,
+    SettingsPage  ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp)
@@ -19,7 +26,11 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    FavoritesPage,
+    LibraryPage,
+    QuotesPage,
+    QuotePage,
+    SettingsPage
   ],
   providers: [
     StatusBar,
